f677808200f8b6686d656a4f33a3895023ed51d3
Anshuman Gahlot <anshumanght@gmail.com>
2026-08-12 11:01:02 +0530

    Update Product Controller

--- project/src/main/java/com/project/controller/ProductController.java
+++ project/src/main/java/com/project/controller/ProductController.java
@@ -1,5 +1,42 @@
 package com.project.controller;
 
-public class ProductController {
+import com.project.entity.Product;
+import com.project.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        return ResponseEntity.ok(productService.createProduct(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productdetails){
+        return ResponseEntity.ok(productService.updateProduct(id,productdetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
 }
