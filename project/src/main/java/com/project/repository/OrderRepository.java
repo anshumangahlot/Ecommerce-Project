620a8a949cc55bfbb376d0f614c75e59827208b8
Anshuman Gahlot <anshumanght@gmail.com>
2026-08-18 02:57:05 +0530

    Add Order repository
    Introduces a new JPA repository for Order entities. It extends JpaRepository and adds a lookup method to fetch all orders for a specific user, enabling order retrieval by associated account.

---
+++ project/src/main/java/com/project/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.project.repository;
+
+import com.project.entity.Order;
+import com.project.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order ,Long> {
+    List<Order> findByUser(User user);
+}
